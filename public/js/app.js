4eecf45614ad18146816d00e8ecc521c9d478b40
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-04 12:25:50 +0100

    exo 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,31 @@
 //EXO 1
 
+// class Personnage {
+//     constructor(age, nom, ville){
+//         this.age = age;
+//         this.nom = nom;
+//         this.ville = ville;
+
+//     }
+
+// }
+
+// let homer = new Personnage(30,'Homer',"Springfield");
+// console.log(homer);
+// let kadri = new Personnage(25,'Kadri',"Bruxelles");
+// console.log(kadri);
+
+
+//EXO 2
+
 class Personnage {
     constructor(age, nom, ville){
         this.age = age;
         this.nom = nom;
         this.ville = ville;
-
+    }
+    sePresenter(){
+        console.log(`Bonjour, je m'appelle ${this.nom}`);
     }
 
 }
@@ -14,3 +34,6 @@ let homer = new Personnage(30,'Homer',"Springfield");
 console.log(homer);
 let kadri = new Personnage(25,'Kadri',"Bruxelles");
 console.log(kadri);
+
+homer.sePresenter();
+kadri.sePresenter();
